35b010fc87a5add1f40112bd9084884079c983c7
Charles Teinturier <teintu.c@gmail.com>
2019-09-23 17:54:11 +0200

    INFRA-699 Remove useless requires

--- lib/puppet/util/network_device/kubernetes/device.rb
+++ lib/puppet/util/network_device/kubernetes/device.rb
@@ -1,10 +1,6 @@
 require 'puppet'
 require 'puppet/util'
 require 'puppet/util/network_device/base'
-require 'puppet/util/network_device/ipcalc'
-require 'puppet/util/network_device/cisco/interface'
-require 'puppet/util/network_device/cisco/facts'
-require 'ipaddr'
 
 class Puppet::Util::NetworkDevice::Kubernetes::Device < Puppet::Util::NetworkDevice::Base
   def initialize(url, options = {})
