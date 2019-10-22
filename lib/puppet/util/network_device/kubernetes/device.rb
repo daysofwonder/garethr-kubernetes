a9057337614960e079c4a321349e855a1236bba3
Brice Figureau <brice@daysofwonder.com>
2019-10-22 17:13:25 +0200

    Add the clustername fact

--- lib/puppet/util/network_device/kubernetes/device.rb
+++ lib/puppet/util/network_device/kubernetes/device.rb
@@ -4,17 +4,27 @@ require 'puppet/util/network_device/kubernetes'
 
 class Puppet::Util::NetworkDevice::Kubernetes::Device
   attr_reader :kubeclient_config
+  attr_reader :cluster_name
 
   def initialize(url, options = {})
     uri = URI.parse(url)
     raise "Device url is not a file:// uri" unless uri.scheme == 'file'
     raise "Device url doesn't exist" unless File.exists?(uri.path)
     @kubeclient_config = uri.path
+    @cluster_name = Puppet.settings[:certname]
+    if uri.request
+      require 'cgi'
+      params = CGI.parse(uri.query)
+      if Array(params['clustername']).flatten.size > 0
+        @cluster_name = params['clustername'].first 
+      end
+    end
   end
 
   def facts
     {
-      'role' => 'k8s'
+      'role' => 'k8s',
+      'clustername' => cluster_name
     }
   end
 end
